dbccb55958af4d623c1485685ca814c1abb30205
guhpissai <gustavopissai3d@gmail.com>
2022-11-04 18:22:07 -0300

    Adicionando paleta com cores distintas e cor preta na primeira cor

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+for (index = 1; index <= 4; index += 1) {
+  const pallete = document.getElementById("color-palette");
+  const colors = document.createElement('div');
+  colors.classList.add("color");
+  pallete.appendChild(colors);
+}
 
-const pallete = document.querySelector("#color-palette");
-const colors = document.createElement('div');
-colors.classList.add("box");
-pallete.appendChild(colors);
+const firtColor = document.querySelector(".color").style.backgroundColor = "black";
+
+const randomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const randomColor = () => [
+  randomNumber(0,255),
+  randomNumber(0,255),
+  randomNumber(0,255)
+];
+
+for (index = 1; index <= 4; index += 1) {
+  const rgb = randomColor();
+  let color = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+  let textColor = color.toString();
+  console.log(textColor);
+  document.querySelectorAll(".color")[index].style.backgroundColor = textColor;
+}
+
+
