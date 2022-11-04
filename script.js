834d0465efba0d4aaf1585b362918fd60b836ee0
guhpissai <gustavopissai3d@gmail.com>
2022-11-04 19:31:09 -0300

    Botao gerador de cores adicionado

--- script.js
+++ script.js
@@ -12,18 +12,27 @@ const randomNumber = (min, max) => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const randomColor = () => [
+
+const randomColor = () => [ 
   randomNumber(0,255),
   randomNumber(0,255),
   randomNumber(0,255)
 ];
 
-for (index = 1; index <= 4; index += 1) {
-  const rgb = randomColor();
-  let color = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
-  let textColor = color.toString();
-  console.log(textColor);
-  document.querySelectorAll(".color")[index].style.backgroundColor = textColor;
+const buttonColor = () => {
+  for (index = 1; index <= 3; index += 1) {
+    const rgb = randomColor();
+    let color = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+    let textColor = color.toString();
+    document.querySelectorAll(".color")[index].style.backgroundColor = textColor;
+  }
 }
+buttonColor();
+
+let colorButton = document.createElement("button");
+colorButton.innerHTML = "Cores aleatórias";
+let body = document.querySelector("body");
+body.appendChild(colorButton);
+colorButton.addEventListener("click", buttonColor);
 
 
