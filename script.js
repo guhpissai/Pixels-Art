fc925e3cf33f31ca57855e8f6bc151e5992973ce
guhpissai <gustavopissai3d@gmail.com>
2022-11-05 13:08:11 -0300

    Corrigindo erro do linter (js)

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-for (index = 1; index <= 4; index += 1) {
-  const pallete = document.getElementById("color-palette");
+for (let index = 1; index <= 4; index += 1) {
+  const pallete = document.getElementById('color-palette');
   const colors = document.createElement('div');
-  colors.classList.add("color");
+  colors.classList.add('color');
   pallete.appendChild(colors);
 }
 
-const firtColor = document.querySelector(".color").style.backgroundColor = "black";
+document.querySelector('.color').style.backgroundColor = 'black';
 
 const randomNumber = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+};
 
-const randomColor = () => [ 
-  randomNumber(0,255),
-  randomNumber(0,255),
-  randomNumber(0,255)
+const randomColor = () => [
+  randomNumber(0, 255),
+  randomNumber(0, 255),
+  randomNumber(0, 255),
 ];
 
 const buttonColor = () => {
-  for (index = 1; index <= 3; index += 1) {
+  for (let index = 1; index <= 3; index += 1) {
     const rgb = randomColor();
-    let color = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
-    let textColor = color.toString();
-    document.querySelectorAll(".color")[index].style.backgroundColor = textColor;
+    const color = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+    const textColor = color.toString();
+    document.querySelectorAll('.color')[index].style.backgroundColor = textColor;
   }
-}
+};
 
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 const bigBox = document.createElement('div');
 body.appendChild(bigBox);
-bigBox.id= "pixel-board";
-for (index = 1; index <= 25; index += 1) {
+bigBox.id = 'pixel-board';
+for (let index = 1; index <= 25; index += 1) {
   const pixel = document.createElement('div');
-  pixel.classList.add("pixel");
+  pixel.classList.add('pixel');
   bigBox.appendChild(pixel);
 }
 
 buttonColor();
 
-let colorButton = document.createElement("button");
-colorButton.id= "button-random-color";
-colorButton.innerHTML = "Cores aleatórias";
+const colorButton = document.createElement('button');
+colorButton.id = 'button-random-color';
+colorButton.innerHTML = 'Cores aleatórias';
 
 body.appendChild(colorButton);
-colorButton.addEventListener("click", buttonColor);
+colorButton.addEventListener('click', buttonColor);
 
 // let storage = () => {
 //   for (let index = 1; index <= 3; index += 1) {
@@ -57,7 +57,6 @@ colorButton.addEventListener("click", buttonColor);
 // }
 // }
 // storage();
-
 
 // let saveColor = () => {
 //   for (index)
