7f5c34ce04d01d280d2fbf3160640563254ca5c3
guhpissai <gustavopissai3d@gmail.com>
2022-11-07 16:44:06 -0300

    Definindo 5x5 elementos

--- script.js
+++ script.js
@@ -31,14 +31,11 @@ const randomColor = () => [
 
 // Criando grade de pixels
 
-const body = document.querySelector('body');
-const bigBox = document.createElement('div');
-body.appendChild(bigBox);
-bigBox.id = 'pixel-board';
+const pixelBoard = document.getElementById('pixel-board');
 for (let index = 1; index <= 25; index += 1) {
   const pixel = document.createElement('div');
   pixel.classList.add('pixel');
-  bigBox.appendChild(pixel);
+  pixelBoard.appendChild(pixel);
 }
 
 // Definindo funcao para o botao gerar cores aleatorias
@@ -59,6 +56,7 @@ const colorButton = document.createElement('button');
 colorButton.id = 'button-random-color';
 colorButton.innerHTML = 'Cores aleatórias';
 
+const body = document.querySelector('body');
 body.appendChild(colorButton);
 colorButton.addEventListener('click', buttonColor);
 
@@ -66,56 +64,47 @@ colorButton.addEventListener('click', buttonColor);
 
 const paletteColors = document.querySelectorAll('.color');
 
-for (let index = 0; index < paletteColors.length; index += 1) {
-  paletteColors[index].addEventListener('click', recebeClick);
+function recebeClick(eventoDeOrigem) {
+  const myTarget = eventoDeOrigem.target;
+  document.querySelector('.selected').classList.remove('selected');
+  myTarget.classList.add('selected');
 }
 
-function recebeClick(eventoDeOrigem) {
-  let myTarget = eventoDeOrigem.target;
-    document.querySelector('.selected').classList.remove('selected');
-    myTarget.classList.add('selected');
+for (let index = 0; index < paletteColors.length; index += 1) {
+  paletteColors[index].addEventListener('click', recebeClick);
 }
 
 // Criando funcao para preencher um pixel com a cor selecionada
 
 const pixelsArt = document.querySelectorAll('.pixel');
 
+function paintPixel(eventOrigin) {
+  const recentColor = document.querySelector('.selected').style.backgroundColor;
+  const myPixelTarget = eventOrigin.target;
+  myPixelTarget.style.backgroundColor = recentColor;
+}
+
 for (let index = 0; index < pixelsArt.length; index += 1) {
   pixelsArt[index].addEventListener('click', paintPixel);
 }
 
-function paintPixel(eventOrigin) {
-  let recentColor = document.querySelector('.selected').style.backgroundColor;
-  let myPixelTarget = eventOrigin.target;
-  myPixelTarget.style.backgroundColor = recentColor;
-}
-
 // Criando botao que retorne a cor do quadro
 
-function clearFrame () {
+function clearFrame() {
   const defaultColor = document.querySelectorAll('.pixel');
-for (i = 0; i < defaultColor.length; i += 1) {
-  defaultColor[i].style.backgroundColor = 'white';
-}
+  for (let i = 0; i < defaultColor.length; i += 1) {
+    defaultColor[i].style.backgroundColor = 'white';
+  }
 }
 clearFrame();
 
 const clearButton = document.getElementById('clear-button');
 const defaultColorButton = document.createElement('button');
 defaultColorButton.id = 'clear-board';
-defaultColorButton.innerHTML = 'Limpar'
+defaultColorButton.innerHTML = 'Limpar';
 clearButton.appendChild(defaultColorButton);
 
 clearButton.addEventListener('click', clearFrame);
-
-
-
-
-
-
-
-
-
 
 // let storage = () => {
 //   for (let index = 1; index <= 3; index += 1) {
