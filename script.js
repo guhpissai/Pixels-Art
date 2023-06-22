f7598667736986e6198dbcf98270853988b5db42
guhpissai <gustavopissai3d@gmail.com>
2023-06-22 19:35:27 -0300

    Implementando funcao para salvar o desenho no local storage

--- script.js
+++ script.js
@@ -63,8 +63,19 @@ const body = document.querySelector('body');
 body.appendChild(colorButton);
 colorButton.addEventListener('click', buttonColor);
 
+function saveDrawing() {
+  let pixelColors = [];
+  const pixels = document.querySelectorAll('.pixel')
+  for(i = 0; i < pixels.length; i++) {
+    pixelColors.push(pixels[i].style.backgroundColor)
+  }
+  localStorage.setItem('drawing', JSON.stringify(pixelColors));
+}
+
 window.onload = function save() {
+  const savedDrawing = JSON.parse(localStorage.getItem('drawing'));
   const savedColors = JSON.parse(localStorage.getItem('colorPalette'));
+  const pixels = document.querySelectorAll('.pixel');
   const colorIndex = document.querySelectorAll('.color');
   if (localStorage.length > 0) {
     for (let index = 0; index < 4; index += 1) {
@@ -74,16 +85,25 @@ window.onload = function save() {
     buttonColor();
     saveColors();
   }
+  if(savedDrawing.length > 0) {
+    for(let i = 0; i < pixels.length; i++) {
+      pixels[i].style.backgroundColor = savedDrawing[i]
+    }
+  }
 };
 
 // Criando grade de pixels
 
 const pixelBoard = document.getElementById('pixel-board');
-for (let index = 1; index <= 25; index += 1) {
-  const pixel = document.createElement('div');
-  pixel.classList.add('pixel');
-  pixelBoard.appendChild(pixel);
+
+function createPixels (length = 25) {
+  for (let index = 1; index <= length; index += 1) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixelBoard.appendChild(pixel);
+  }
 }
+createPixels()
 
 // Criando funcao para selecionar cor na paleta
 
@@ -101,17 +121,21 @@ for (let index = 0; index < paletteColors.length; index += 1) {
 
 // Criando funcao para preencher um pixel com a cor selecionada
 
-const pixelsArt = document.querySelectorAll('.pixel');
 
 function paintPixel(eventOrigin) {
   const recentColor = document.querySelector('.selected').style.backgroundColor;
   const myPixelTarget = eventOrigin.target;
   myPixelTarget.style.backgroundColor = recentColor;
+  saveDrawing();
 }
 
-for (let index = 0; index < pixelsArt.length; index += 1) {
-  pixelsArt[index].addEventListener('click', paintPixel);
+function paint () {
+  const pixelsArt = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixelsArt.length; index += 1) {
+    pixelsArt[index].addEventListener('click', paintPixel);
+  }
 }
+paint();
 
 // Criando botao que retorne a cor do quadro
 
@@ -120,13 +144,48 @@ function clearFrame() {
   for (let i = 0; i < defaultColor.length; i += 1) {
     defaultColor[i].style.backgroundColor = 'white';
   }
+  saveDrawing();
 }
-clearFrame();
 
 const clearButton = document.getElementById('clear-button');
 const defaultColorButton = document.createElement('button');
 defaultColorButton.id = 'clear-board';
 defaultColorButton.innerHTML = 'Limpar';
+
+const boardSizeButton = document.createElement('input');
+boardSizeButton.setAttribute('type', 'number');
+boardSizeButton.setAttribute('min', '1');
+boardSizeButton.id = 'board-size';
+boardSizeButton.innerHTML = ''
+
+const vqmButton = document.createElement('button');
+vqmButton.id = 'generate-board';
+vqmButton.innerHTML = 'VQV';
+
 clearButton.appendChild(defaultColorButton);
+clearButton.appendChild(boardSizeButton);
+clearButton.appendChild(vqmButton);
+
+
+function attPixels () {
+  let boardSize = boardSizeButton.value * boardSizeButton.value;
+  if(!boardSize) {
+    return window.alert('Board inválido!')
+  }
+  const pixels = document.querySelectorAll('.pixel');
+  for(i = 0; i < pixels.length; i++) {
+    pixels[i].remove()
+  }
+  for(i = 0; i < boardSize; i++) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixelBoard.appendChild(pixel);
+  }
+  let boardPixel = boardSizeButton.value * 42
+  pixelBoard.style.maxWidth = `${boardPixel}px`;
+  paint();
+}
+
+vqmButton.addEventListener('click', attPixels)
 
 clearButton.addEventListener('click', clearFrame);
